b9b6125bebb6b9f62eee038155ff4124861d4ae6
yfukagawa <yfukagawa@rooseveltinnovations.com>
2024-05-01 08:31:04 -0400

    ttt update

---
+++ tictactoePython/tictactoe.py
@@ -0,0 +1,12 @@
+slots = [1,2,3,4,5,6,7,8,9]
+
+print('-','-','-','-','-','-','-',)
+print('|',slots[0], '|',slots[1],'|',  slots[2],'|')
+print('-','-','-','-','-','-','-',)
+print('|',slots[3], '|',slots[4],  '|',  slots[5],'|')
+print('-','-','-','-','-','-','-',)
+print('|',slots[6],  '|',  slots[7],  '|',  slots[8],'|')
+print('-','-','-','-','-','-','-',)
+
+player1 = input("Select the slot by entering a number 1 - 9: ")
+print("Player 1 selected: " + player1)
